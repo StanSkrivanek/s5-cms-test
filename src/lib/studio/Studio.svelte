f590015a6b3c5d0c732fbdf4b61bef66dbe968b3
Stan Skrivanek <s.skrivanek@icloud.com>
2025-04-06 00:18:47 +0100

    Remove embedded Git repository and re-add studio directory

---
+++ src/lib/studio/Studio.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { renderStudio } from 'sanity';
+	import { onMount } from 'svelte';
+	import config from './sanity.config';
+
+	/**
+	 * @type {HTMLDivElement}
+	 */
+	let studioRoot: HTMLDivElement;
+
+	onMount(() => {
+		if (studioRoot) {
+			// Use renderStudio instead of React's createRoot
+			const cleanupFn = renderStudio(studioRoot, config);
+
+			// Return cleanup function for when component is destroyed
+			return () => {
+				cleanupFn();
+			};
+		}
+	});
+</script>
+
+<div bind:this={studioRoot} style="height: 100vh;"></div>
