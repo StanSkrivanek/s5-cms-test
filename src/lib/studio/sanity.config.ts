b84d277114f811b2006c740004625d2a681190e1
Stan Skrivanek <s.skrivanek@icloud.com>
2025-04-06 07:42:44 +0100

    add websocket version in sanity config

--- src/lib/studio/sanity.config.ts
+++ src/lib/studio/sanity.config.ts
@@ -8,6 +8,9 @@ export default defineConfig({
 	projectId: import.meta.env.VITE_PUBLIC_SANITY_PROJECT_ID,
 	dataset: import.meta.env.VITE_PUBLIC_SANITY_DATASET || 'production',
 	apiVersion: import.meta.env.VITE_PUBLIC_SANITY_API_VERSION || '2023-05-03',
+	websocketOptions: {
+		apiVersion: '2022-06-30'
+	},
 	title: 'Beloved recipes',
 	description: 'A Sanity Studio for Beloved Recipes',
 	plugins: [structureTool(), visionTool()],
