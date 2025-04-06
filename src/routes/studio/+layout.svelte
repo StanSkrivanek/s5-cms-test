79cb0d891fb19b4c64843d68d7e4b4201c3498dc
Stan Skrivanek <s.skrivanek@icloud.com>
2025-04-06 19:38:10 +0100

    min changes

--- src/routes/studio/+layout.svelte
+++ src/routes/studio/+layout.svelte
@@ -1,12 +1,17 @@
 <script>
     import Studio from '$lib/studio/Studio.svelte';
-//  props we can access {children, data, form} 
-let { children } = $props();
-// import '../app.css';
+    import { browser } from '$app/environment';
+    // props we can access {children, data, form} 
+    // let { children } = $props();
+    // import '../app.css';
+    // children = children || (() => null);
 </script>
 
-<Studio />
- <!-- only to avoid the server error that `@render` is not found -->
-  <!-- children?.() -> optional only if children exists -->
 
-{@render children?.()}
+
+    <Studio />
+
+ <!-- only to avoid the server error that `@render` is not found -->
+  <!-- children?.() -> optional only if children exists, preventing errors when it's null or undefined -->
+
+
